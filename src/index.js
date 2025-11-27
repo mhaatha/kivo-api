7421aa484e62b7f7003daa3d7fc5c2e841c796df
Hafidz Athaya <mhafidza528@gmail.com>
2025-11-27 23:02:23 +0800

    initial project setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const helmet = require('helmet');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
-app.use(morgan('dev'));
-app.use(cors());
-app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
