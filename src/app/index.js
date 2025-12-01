cf07a72075e8fdcd89340e249793964149a54eef
Hafidz Athaya <mhafidza528@gmail.com>
2025-12-01 13:55:14 +0800

    add: mount the better auth handler

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import { toNodeHandler } from 'better-auth/node';
+import { auth } from './utils/auth.js';
 
 const app = express();
+
+app.all('/api/v1/auth/*splat', toNodeHandler(auth));
 
 // Middleware JSON parser
 // It helps the app read JSON data sent from the client
 // and makes it available in req.body
+//
+// Don’t use express.json() before the Better Auth handler.
+// Use it only for other routes, or the client API will get stuck on "pending".
 app.use(express.json());
 
 export default app;
