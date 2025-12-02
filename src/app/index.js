83d5753654f1e68353e83523356f091ce21a1ce5
Hafidz Athaya <mhafidza528@gmail.com>
2025-12-03 00:03:38 +0800

    fix: cors issue

--- src/app/index.js
+++ src/app/index.js
@@ -9,7 +9,7 @@ const app = express();
 // Configure CORS middleware
 app.use(
   cors({
-    origin: ['*'],
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   }),
