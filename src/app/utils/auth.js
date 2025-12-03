b736e3e762bb7326d12d464698e28c3e3e5f4850
Hafidz Athaya <mhafidza528@gmail.com>
2025-12-03 11:02:00 +0800

    add: setup mongodb connection

--- src/app/utils/auth.js
+++ src/app/utils/auth.js
@@ -1,29 +1,30 @@
 import { betterAuth } from 'better-auth';
-import { MongoClient } from 'mongodb';
 import { mongodbAdapter } from 'better-auth/adapters/mongodb';
 import { openAPI } from 'better-auth/plugins';
 import { nextCookies } from 'better-auth/next-js';
-
-const client = new MongoClient(process.env.MONGODB_URI);
-const db = client.db();
+import { client, db } from './mongodb.js';
 
 export const auth = betterAuth({
-  database: mongodbAdapter(db, {
-    client,
-  }),
+  database: mongodbAdapter(db, { client }),
+
   experimental: {
     joins: true,
   },
+
   emailAndPassword: {
     enabled: true,
   },
+
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     },
   },
+
   basePath: '/api/v1/auth',
+
   plugins: [openAPI(), nextCookies()],
+
   trustedOrigins: ['http://localhost:3000', 'http://127.0.0.1:3000'],
 });
