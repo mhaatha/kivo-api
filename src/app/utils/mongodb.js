e797d1a2abacbe6800630e3541dbb5e7389f2daa
Hafidz Athaya <mhafidza528@gmail.com>
2025-12-03 11:02:00 +0800

    add: setup mongodb connection

---
+++ src/app/utils/mongodb.js
@@ -0,0 +1,19 @@
+import { MongoClient } from 'mongodb';
+
+const uri = process.env.MONGODB_URI;
+if (!uri) {
+  throw new Error('error: MONGODB_URI is not defined');
+}
+
+// Create one MongoClient instance only
+const client = new MongoClient(uri, {
+  maxPoolSize: 10,
+  serverSelectionTimeoutMS: 5000,
+});
+
+// We DO NOT call client.connect() here.
+// Better Auth will handle connection lazily.
+
+const db = client.db();
+
+export { client, db };
