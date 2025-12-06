0e583d0bab93df1e37e1cd3b553f9a95a3aa5ff8
Vito Andareas Manik <vitoandareas15@gmail.com>
2025-12-06 22:29:24 +0800

    chore(app): update CORS environment variable naming
    - Rename FRONTEND_URL1 to WEB_URL for consistency and clarity
    - Improve environment variable naming convention across the application

--- src/app/index.js
+++ src/app/index.js
@@ -14,7 +14,7 @@ app.use(
     origin: [
       'http://localhost:3000',
       'http://127.0.0.1:3000',
-      process.env.FRONTEND_URL1,
+      process.env.WEB_URL,
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
